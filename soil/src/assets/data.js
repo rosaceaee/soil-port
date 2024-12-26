6fe5833c52a5ed4e9e7afe25cda04c7d41b240be
장영주 <yjjang2@the-51.com>
2024-12-26 15:09:03 +0900

    update json structure

--- soil/src/assets/data.js
+++ soil/src/assets/data.js
@@ -1,14 +1,185 @@
 export const data = {
-  test: [
+  career: [
     {
       id: 1,
-      title: "Test 1",
-      content: "This is a test content",
+      companyName: "Test 1",
+      date: "2024.5. ~ 현재",
+      totalDate: "1 years",
+      projList: [
+        {
+          listIdx: 1,
+          projName: "미래에셋생명",
+          imgUrl: "/img/lge1.png",
+          url: "url",
+          desc: [
+            "작업 생산성을 높이기 위하여 컴포넌트 CSS, JS 레거시 코드 수정 후 컴포넌트 활용",
+            "단위, 통합테스트 대응하여 ui 화면 개선",
+            "イベントページの簡単ゲームのコード仕組み作成などの簡単なイベント関数の運用",
+          ],
+          skill: ["js", "scss", "html", "figma"],
+        },
+        {
+          listIdx: 2,
+          projName: "LG global platform",
+          imgUrl: "/img/lge1.png",
+          url: "url",
+          desc: [
+            "헝가리, 오스트리아 사이트 UIUX 구축 및 운영",
+            "결제 모듈 ui영역 개선 담당",
+            "기존 ui관련 레거시 코드 개선",
+            "다른 다국어 사이트와 균일한 ui 구현을 위하여 컴포넌트 디자인 개선",
+          ],
+          skill: ["js", "scss", "pug"],
+        },
+        {
+          listIdx: 3,
+          projName: "현대자동차 N브랜드 사이트 리뉴얼",
+          imgUrl: "/img/lge1.png",
+          url: "url",
+          desc: [
+            "ui디자인 일관성과 생산성을 높이기 위한 컴포넌트 시스템 개선 및 참여",
+            "단위, 통합테스트 대응하여 ui 화면 개선",
+            "다국어 사이트 반영 담당",
+          ],
+          skill: ["js", "scss", "html"],
+        },
+      ],
     },
     {
       id: 2,
-      title: "Test 2",
-      content: "This is another test content",
+      companyName: "주식회사 클라우봇",
+      date: "2021.12. ~ 2023.07.",
+      totalDate: "(1년 7개월, 풀타임)",
+      projList: [
+        {
+          listIdx: 1,
+          projName: "최선어학원 creo 유지보수",
+          url: "url",
+          desc: [
+            "유지보수, 테스트",
+            " 기존 ui 스타일 소스 레거시 정리 및 코드 경제?사용?성 개선",
+            "ui 테스트 작업 대응하여 디바이스 최적화로 사용자 학습도움에 기여",
+          ],
+          skill: ["react", "reactNative", "scss"],
+        },
+        {
+          listIdx: 2,
+          projName: "metaexam 영어시험 구축",
+          url: "url",
+          desc: [
+            "퍼블리싱, 테스트",
+            "수험자의 이해를 돕기 위한 uiux 디자인 참여. 직관적인 스타일로 작업함",
+          ],
+          skill: ["react", "scss", "antd"],
+        },
+        {
+          listIdx: 3,
+          projName: " phonics monster 교육 툴 유지보수",
+          url: "url",
+          desc: [
+            "퍼블리싱, 테스트",
+            "css 레거시 코드 정리 및 전체 소스 리팩토링",
+            "ui 테스트 작업 대응하여 디바이스 최적화 작업",
+          ],
+          skill: ["js", "css", "html"],
+        },
+        {
+          listIdx: 4,
+          projName: "루시드 영어",
+          url: "url",
+          desc: [
+            "학원 교육 부교재용 앱 구축",
+            "컴포넌트 UI 구현, 디바이스 별 반응형 구현",
+            "ui 테스트 작업 대응하여 디바이스 최적화 작업",
+          ],
+          skill: ["react", "reactNative", "scss"],
+        },
+      ],
+    },
+    {
+      id: 3,
+      companyName: "잡코리아",
+      date: " 2019.7. ~ 2021.3.",
+      totalDate: "(1년 8개월, 풀타임)",
+      projList: [
+        {
+          listIdx: 1,
+          projName: "구인구직 공고 페이지 운영",
+          url: "url",
+          desc: ["구인구직 공고페이지 운영 및 공고 관리"],
+          skill: ["css", "html"],
+        },
+      ],
+    },
+    {
+      id: 4,
+      companyName: "리눅스웨어",
+      date: "2019.2. ~ 2019.5.",
+      totalDate: "(3개월, 아르바이트)",
+      projList: [
+        {
+          listIdx: 1,
+          projName: "서울여자대학교 아동학과",
+          url: "url",
+          desc: ["desc1", "desc2", "desc3"],
+          skill: ["js", "css", "html"],
+        },
+        {
+          listIdx: 2,
+          projName: "ssmedipia",
+          url: "url",
+          desc: ["desc1", "desc2", "desc3", "desc4"],
+          skill: ["js", "css", "html"],
+        },
+        {
+          listIdx: 3,
+          projName: "파란관세사무소",
+          url: "url",
+          desc: ["desc1", "desc2", "desc3", "desc4"],
+          skill: ["vanilla js", "scss"],
+        },
+      ],
+    },
+  ],
+  others: [
+    {
+      id: 1,
+      projName: "privateWork",
+      projList: [
+        {
+          listIdx: 1,
+          projName: "projname1",
+          url: "url",
+          desc: ["desc1", "desc2", "desc3"],
+          skill: ["react", "reactNative", "scss", "postman"],
+        },
+      ],
+    },
+    {
+      id: 2,
+      projName: "MDN 웹 문서 영한번역 기여",
+      projList: [
+        {
+          listIdx: 1,
+          projName: "outerHeight",
+          url: "https://developer.mozilla.org/ko/docs/Web/API/Window/outerHeight",
+        },
+        {
+          listIdx: 2,
+          projName: "device memory api",
+          url: "https://developer.mozilla.org/ko/docs/Web/API/Device_Memory_API",
+        },
+        {
+          listIdx: 3,
+          projName: "tabindex",
+          url: "https://developer.mozilla.org/ko/docs/Web/API/HTMLElement/tabIndex",
+        },
+        {
+          listIdx: 4,
+          projName: "clipboard",
+          url: "https://developer.mozilla.org/ko/docs/Web/API/Navigator/clipboard",
+        },
+      ],
     },
   ],
 };
