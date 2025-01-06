d8efab9f3a406c24ababa841eabce75f3c9e3c8f
장영주 <yjjang2@the-51.com>
2025-01-06 11:21:05 +0900

    update ui (fixed to use a Bottom not Contents)

--- soil/src/assets/Temp.js
+++ soil/src/assets/Temp.js
@@ -7,7 +7,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
+export const Temp = ({ onMouseEnter, onMouseLeave }) => {
   const [isHovered, setIsHovered] = useState({});
   const [up, setUp] = useState(0);
   const [workPosition, setWorkPosition] = useState(-180 * 16);
@@ -69,7 +69,6 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
 
   const boxRef1 = useRef(null);
   const containerRef = useRef(null);
-  const navWrapRef = useRef(null);
 
   useEffect(() => {
     const setAnimation = () => {
@@ -112,6 +111,32 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
       boxItems.revert();
     };
   }, []);
+
+  const navWrapRef = useRef(null);
+
+  //   useEffect(() => {
+  //     const container = containerRef.current;
+  //     const elements = container?.querySelectorAll(".wrap *, .work-con *");
+
+  //     const observer = new IntersectionObserver(
+  //       (entries) => {
+  //         entries.forEach((entry) => {
+  //           entry.target.classList.toggle("cursor-hover", entry.isIntersecting);
+  //         });
+  //       },
+  //       {
+  //         root: null,
+  //         rootMargin: "0px",
+  //         threshold: 0,
+  //         // nav-wrap을 intersection root로 지정
+  //         root: navWrapRef.current,
+  //       }
+  //     );
+
+  //     elements?.forEach((element) => observer.observe(element));
+
+  //     return () => elements?.forEach((element) => observer.unobserve(element));
+  //   }, []);
 
   useEffect(() => {
     const container = containerRef.current;
@@ -177,36 +202,11 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
     };
   }, []);
 
-  // useEffect(() => {
-  //   let mm = gsap.matchMedia();
-  //   const boxItems = gsap.context((self) => {
-  //     const boxes = self.selector(".ani");
-  //     boxes.forEach((box) => {
-  //       gsap.from(box, {
-  //         y: 300,
-  //         opacity: 0,
-  //         stagger: 1,
-  //         scrollTrigger: {
-  //           trigger: box,
-  //           start: "top bottom", // 셀렉터로 등록한 요소의 상단이 뷰포트의 바닥에 있을 때 시작
-  //           end: "top 5%", // 상단 20프로에서 완료
-  //           scrub: true, // 하위요소를 하나씩 순차적으로 하고 싶어서 등록
-  //         },
-  //       });
-  //       gsap.to(box, {
-  //         y: 0,
-  //         opacity: 0,
-  //       });
-  //     });
-  //   }, boxRef1); // ref 값은 scope로만 등록한다
-  //   return () => boxItems.revert(); // clean up
-  // }, []); // <- Scope!
-
   return (
     <>
       <article className="container" ref={containerRef}>
         <div className="wrap">
-          <section className="info">
+          <section className={isHovered ? "info " : "info cursor-hover"}>
             {/* <img src={require("../img/sprout-sm.png")} alt="" /> */}
             <div className="box">
               <div className="row">
@@ -217,7 +217,7 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                     onMouseEnter={(e) => handleMouseEnter("stack1", e)}
                     onMouseLeave={(e) => handleMouseLeave("stack1", e)}
                   >
-                    안녕하세요
+                    .
                   </h1>
                 </div>
                 <div className="col"></div>
@@ -227,39 +227,14 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                 <div className="col desc center">
                   <div className="cont-left">
                     <span className="up">
-                      {up === 1 ? "자주적인" : null}
-                      {up === 2 ? "호기심 많은" : null}
+                      {up === 1 ? "" : null}
+                      {up === 2 ? " 많은" : null}
                       {up === 3 ? "" : null} <span>MONO</span>
                     </span>
                   </div>
                   <div className="cont-right">
                     <div className="col desc">
-                      <span>
-                        {/* 他には
-                        「自分のスキルで他人に役に立つプロダクトを作る」を目指しています。
-                        ですので、ユーザー目線で重視しつつ仕上げしたいと思います。
-                        <br />
-                        なお、学習者のため勉強しやすくするためにウェブサイトの翻訳にも興味があって、
-                        こんちゅりびゅーとしてます。 */}
-                        <h3>
-                          나누는 것을 좋아합니다. 새로운 정보와 즐거운 것들을요.
-                        </h3>
-                        React와 vanilla Js로 웹 퍼블리싱과 프론트작업을 하며
-                        중간 역할과 더불어 UIUX개선을 하며 프로젝트를 성공적으로
-                        완수하였습니다.
-                        <br />
-                        혼자만 아는 것보다 스스로의 능력이 사람들에게 도움이
-                        되길 바라며 웹 문서의 영한번역에도 기여하고 있습니다.
-                        <br />
-                        <br />
-                        기획자와 디자이너 사이에서 비개발 직군에게도 이해하기
-                        쉽게 설명하여 원활한 작업진행에 도움이 되는 것에 보람을
-                        느낍니다.
-                        <br />
-                        인터랙션, 심미성이 높은 UI 구현과 더불어 사용자 경험의
-                        향상, 그리고 변화하는 개발 생태계에서 끊임없이 배우며
-                        같이 일하고 싶은 개발자가 되고 싶습니다.
-                      </span>
+                      <span>1</span>
                     </div>
                   </div>
                 </div>
@@ -285,30 +260,24 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                     <div className="curr">
                       <ul>
                         <li>
-                          <p className="tit">frentend dev? tools</p>
-                          <p>
-                            Javascript(ES6), React, ReactNative, Html5, Pug,
-                            Postman, GitHub/GitLab
-                          </p>
+                          <p className="tit">1111</p>
+                          <p>1111</p>
                         </li>
                         <li>
-                          <p className="tit">UI libraries</p>
-                          <p>
-                            CSS3/SCSS/SASS, Tailwind CSS, Styled Components, Ant
-                            design, Bootstrap.
-                          </p>
+                          <p className="tit">2213124</p>
+                          <p>11111</p>
                         </li>
                         <li>
-                          <p className="tit">design tools</p>
-                          <p>Figma, Adobe products </p>
+                          <p className="tit">2213124 tools</p>
+                          <p>2213124 </p>
                         </li>
                         <li>
-                          <p className="tit">Interested in...</p>
-                          <p>Python, </p>
+                          <p className="tit">2213124 in...</p>
+                          <p>2213124, </p>
                         </li>
                         <li>
-                          <p className="tit">Also available...</p>
-                          <p>English, 日本語 </p>
+                          <p className="tit">2213124 2213124...</p>
+                          <p>2213124, 2213124 </p>
                         </li>
                       </ul>
                     </div>
@@ -351,7 +320,7 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                         <div className="proj-box">
                           <div className="contents">
                             <div className="company-info">
-                              <h2>{list.companyName}</h2>
+                              <h2>asdf</h2>
                               <p>
                                 {list.date} <br /> {list.totalDate}
                               </p>
@@ -363,26 +332,21 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                                   return (
                                     <>
                                       <div className="proj-info">
-                                        <img
-                                          src={project.imgUrl}
-                                          className="pic"
-                                          alt=""
-                                        />
                                         <h3 className="txt-proj-name">
-                                          {project.projName}
+                                          2213124
                                         </h3>
                                         <a
                                           href={`${project.url}`}
                                           target="_blank"
                                         >
-                                          링크
+                                          2213124
                                         </a>
 
                                         <div className="proj-info__desc">
                                           {project.desc.map(
                                             (descItm, index) => {
                                               return (
-                                                <p key={index}>{descItm}</p>
+                                                <p key={index}>dddddddd</p>
                                               );
                                             }
                                           )}
@@ -396,7 +360,7 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                                                     key={index}
                                                     className={`label ${skillItm}`}
                                                   >
-                                                    {chipSkill}
+                                                    2213124
                                                   </span>
                                                 );
                                               }
@@ -437,15 +401,12 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                   <div className="proj-box">
                     <div className="contents">
                       <div>
-                        <h2>개인 프로젝트</h2>
+                        <h2>2213124 2213124</h2>
                       </div>
                       <ul>
                         <li className="desc-wrapp">
                           <h3 className="txt-proj-name">ㅇㅇ</h3>
-                          <p>
-                            가벼운 원페이지로 작업. 숫자 입력하면 단번에 알 수
-                            있도록.
-                          </p>
+                          <p>2213124</p>
                           <p>링크</p>
                         </li>
                       </ul>
@@ -470,9 +431,7 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                       </div>
                       <ul>
                         <li>
-                          <h3 className="txt-proj-name">
-                            MDN 웹 문서 영한번역 기여
-                          </h3>
+                          <h3 className="txt-proj-name">2213124</h3>
                         </li>
                         {data.others[1].projList.map((list, index) => {
                           return (
@@ -483,15 +442,13 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                                 href=""
                                 target="_blank"
                               >
-                                {list.url}
+                                2213124
                               </a>
                             </li>
                           );
                         })}
                         <li>
-                          <h3 className="txt-proj-name">
-                            regexlearn 한국어 번역 기여
-                          </h3>
+                          <h3 className="txt-proj-name">2213124</h3>
                         </li>
                         {data.others[2].projList.map((list, index) => {
                           return (
@@ -500,7 +457,7 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
                                 <a href="" target="_blank">
                                   {list.url}
                                 </a>
-                                <p>{list.projName}</p>
+                                <p>2213124</p>
                               </span>
                             </li>
                           );
@@ -518,8 +475,12 @@ export const Bottom = ({ onMouseEnter, onMouseLeave }) => {
         {/****************** works end *******************/}
       </article>
 
-      <nav onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        <div className="nav-wrap" ref={navWrapRef}>
+      <nav
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        ref={navWrapRef}
+      >
+        <div className="nav-wrap">
           <span onClick={() => scrollToSection("skill")}>1</span>
           <span onClick={() => scrollToSection("work-container")}>1</span>
           <span>
