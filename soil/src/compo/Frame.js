d1f8ccf9c8d2da3005313487793ea5556cbe80eb
j <115390541+rosaceaee@users.noreply.github.com>
2025-05-06 17:20:03 +0900

    ui error fix

--- soil/src/compo/Frame.js
+++ soil/src/compo/Frame.js
@@ -1,27 +1,12 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
-
+import React from "react";
 import styled from "styled-components";
 
 export const Row = styled.div`
   display: grid;
   grid-template-columns: 13% 1fr 13%;
   width: 100%;
-  box-sizing: border-box;
-  overflow-x: hidden;
-
-  @media screen and (max-width: 768px) {
-    grid-template-columns: 10% 1fr 10%;
-  }
-
-  @media screen and (max-width: 500px) {
-    grid-template-columns: 5% 1fr 5%;
-  }
 `;
 
 export const Col = styled.div`
-  flex: ${(props) => props.flex || "1"};
-  max-width: ${(props) => props.maxWidth || "100%"};
-  box-sizing: border-box;
   width: 100%;
-  overflow-x: hidden;
 `;
